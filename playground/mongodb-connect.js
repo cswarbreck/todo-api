2757c9c27ef05b8a4a34bee19c67f6468b87dc74
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-25 17:08:00 +0100

    added second db update users

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,13 +8,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
     const db = client.db('TodoApp');
 
     db.collection('Todos').insertOne({
-
+        text: 'Something to do',
+        completed: false
     }, (err, result) =>{
         if (err) {
             return console.log('Unable to insert', err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
-    })
+    });
+
+    db.collection('Users').insertOne({
+        name: 'Cosmo',
+        age: '28',
+        location: 'London',
+        broke: true
+    }, (err, result)=>{
+        if(err){
+            return console.log('Unable to insert', err);
+        } 
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
 
     client.close();
 });
