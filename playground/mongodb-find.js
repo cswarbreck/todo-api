3bb5954cd397cd59e653702f71b2908e6cf4ac72
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-25 19:31:44 +0100

    updated to include server files

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,39 +1,30 @@
-//const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
-//We use this to select a particular value, in this case, ObjectedID, which is called later on.
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client)=>{
-    if (err) {
-        return console.log('Unable to connect')
-    } 
-    console.log('Connected to server');
-    const db = client.db('TodoApp');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-    /* db.collection('Todos').find({completed: false}).toArray().then((docs)=>{
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2))
-    })
-    .catch(err =>{
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('57bb36afb3b6a3801d8c479d')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-        console.log('error - content not updated', err);
-    }); */
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
 
-   /*  db.collection('Todos').find({
-        _id: new ObjectID('5ae0aa6dc24ca756f8230689')
-    }).toArray().then((docs)=>{
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2))
-    })
-    .catch(err =>{
+  db.collection('Users').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  });
 
-        console.log('error - content not updated', err);
-    }); */
-
-    db.collection('Users').find({name: 'Cosmo'}).toArray().then((docs)=>{
-        console.log(JSON.stringify(docs, undefined, 2));
-    });
-
-   
-
-    //client.close();
+  // db.close();
 });
